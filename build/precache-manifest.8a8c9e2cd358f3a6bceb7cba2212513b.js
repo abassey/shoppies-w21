94d658234d38928aa58c4674e4371fbc924be5f4
Anemmeabasi Bassey <anemmeabasi1@gmail.com>
2020-09-03 16:59:54 -0400

    Add files via upload

---
+++ build/precache-manifest.8a8c9e2cd358f3a6bceb7cba2212513b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb94d0afe441b4a3841a6c2e3cab55c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3957da073829ace6af3c",
+    "url": "/static/css/2.31bf72a5.chunk.css"
+  },
+  {
+    "revision": "16e7cf2bcfcbfa95fcbc",
+    "url": "/static/css/main.1e1d860b.chunk.css"
+  },
+  {
+    "revision": "3957da073829ace6af3c",
+    "url": "/static/js/2.411f007c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.411f007c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e7cf2bcfcbfa95fcbc",
+    "url": "/static/js/main.2b701342.chunk.js"
+  },
+  {
+    "revision": "3c723e48decc6f6f72c1",
+    "url": "/static/js/runtime-main.30aace72.js"
+  }
+]);
